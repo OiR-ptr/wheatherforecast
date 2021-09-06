762d2e9b00ce4b11a0edae572da996b18fbad852
OiR-ptr <myr.ryusuke@gmail.com>
2021-09-07 02:11:06 +0900

    :+1: add initializing for weather const data

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,47 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <weather-forecast :weather-codes="weatherCodes" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import WeatherForecast from './components/WeatherForecast.vue'
 
 export default {
   name: 'App',
+  data: () => {
+    return {
+      weatherCodes: {},
+      officeCodes: {},
+    };
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    WeatherForecast,
+  },
+  created() {
+    this.initializeTelops();
+    this.initializeOffices();
+  },
+  methods: {
+    initializeTelops() {
+      let self = this;
+      fetch(`TELOPS.json`).then(data => {
+        return data.json();
+      }).then(telops => {
+        self.$set(self, 'weatherCodes', telops);
+      }).catch(error => console.error(error))
+    },
+    initializeOffices() {
+      let self = this;
+      fetch(`https://www.jma.go.jp/bosai/common/const/area.json`).then(data => {
+        return data.json();
+      }).then(area => {
+        self.$set(self, 'officeCodes', area);
+      }).catch(error => console.error(error));
+    }
   }
 }
 </script>
---
+++ src/components/WeatherForecast.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Weather Forecast</h1>
+    <p>{{this.weatherCodes}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WeatherForecast',
+  props: {
+    weatherCodes: {
+      type: Object,
+      default: () => {
+        return { item: 'baka' };
+      }
+    },
+  },
+  updated: {
+    weatherCodes() {
+      debugger;
+      console.log(this.weatherCodes);
+    }
+  }
+}
+</script>
